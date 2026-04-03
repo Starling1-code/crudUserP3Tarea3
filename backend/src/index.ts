a6a9ae26a84c2acf02ee4fb5eb3f8f17a113cf52
Starling Rosario <rosariofranco1720@gmail.com>
2026-04-03 11:27:50 -0400

    auth-jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
